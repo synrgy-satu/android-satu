f3857023e80a76ac7b1c62bc97cf062a16810391
pierreviences <imaniyazid@gmail.com>
2024-08-05 21:00:52 +0700

    feat: add logic splashscreen

--- app/src/main/java/com/example/satu/ui/activities/auth/newuser/onboarding/OnBoardingNewUserActivity.kt
+++ app/src/main/java/com/example/satu/ui/activities/auth/newuser/onboarding/OnBoardingNewUserActivity.kt
@@ -1,21 +1,30 @@
 package com.example.satu.ui.activities.auth.newuser.onboarding
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.satu.R
+import com.example.satu.databinding.ActivityOnBoardingNewUserBinding
+import com.example.satu.ui.activities.auth.newuser.login.LoginEmailActivity
+import com.example.satu.ui.activities.auth.newuser.register.RegisterEmailActivity
 
 class OnBoardingNewUserActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnBoardingNewUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_on_boarding_new_user)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityOnBoardingNewUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupClickListeners()
+    }
+    private fun setupClickListeners() = with(binding) {
+        btnLoginOnboarding.setOnClickListener {
+            startActivity(Intent(this@OnBoardingNewUserActivity, LoginEmailActivity::class.java))
+        }
+        btnRegisOnboarding.setOnClickListener {
+            startActivity(Intent(this@OnBoardingNewUserActivity, RegisterEmailActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/example/satu/ui/activities/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/example/satu/ui/activities/splash/SplashScreenActivity.kt
@@ -1,21 +1,25 @@
 package com.example.satu.ui.activities.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.satu.R
+import com.example.satu.ui.activities.auth.newuser.onboarding.OnBoardingNewUserActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_DISPLAY_LENGTH = 3000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Handler().postDelayed({
+            val mainIntent = Intent(this@SplashScreenActivity, OnBoardingNewUserActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, SPLASH_DISPLAY_LENGTH)
     }
 }
