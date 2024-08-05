b0bd9a90226099b187313907978119ecf898b5e1
pierreviences <imaniyazid@gmail.com>
2024-08-05 21:11:11 +0700

    chore: update ui login new user

--- app/src/main/java/com/example/satu/ui/activities/auth/newuser/login/LoginEmailActivity.kt
+++ app/src/main/java/com/example/satu/ui/activities/auth/newuser/login/LoginEmailActivity.kt
@@ -1,21 +1,38 @@
 package com.example.satu.ui.activities.auth.newuser.login
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.satu.R
+import com.example.satu.databinding.ActivityLoginEmailBinding
+import com.example.satu.databinding.ActivityOnBoardingNewUserBinding
 
 class LoginEmailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginEmailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login_email)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityLoginEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val fullText = "Masukkan Email dan Kata Sandi untuk mengakses M-Banking SATU"
+        val spannableString = SpannableString(fullText)
+
+        val primaryColor = ContextCompat.getColor(this, R.color.primary)
+        val startIndex = fullText.indexOf("M-Banking SATU")
+        val endIndex = startIndex + "M-Banking SATU".length
+
+        spannableString.setSpan(
+            ForegroundColorSpan(primaryColor),
+            startIndex,
+            endIndex,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        binding.tvText5.text = spannableString
     }
 }
