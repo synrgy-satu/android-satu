412f9c2df0075b87658765f495eee6198b4ee06c
Kunti Najma Jalia <najmajalia02@gmail.com>
2024-08-23 23:14:40 +0700

    feat: mutasi (periode)

--- app/src/main/java/com/example/satu/mutation/network/Config.kt
+++ app/src/main/java/com/example/satu/mutation/network/Config.kt
@@ -2,7 +2,7 @@ package com.example.satu.mutation.network
 
 object Config {
     // Replace with your actual Bearer token
-    private const val BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInVzZXJfbmFtZSI6InRhdWZpcV9pbXJvbiIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdLCJleHAiOjE3MjQ0MTQ5ODEsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiIsIlJPTEVfUkVBRCIsIlJPTEVfV1JJVEUiXSwianRpIjoiY2xSTnlrdWJTLTlUOWY1U0tVVkQ1Z0NmMjQ4IiwiY2xpZW50X2lkIjoibXktY2xpZW50LXdlYiJ9.kH2qlibDxeGFAIMvHz57AzXGjaM4lO4XuHbe1HlE3g0"
+    private const val BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInVzZXJfbmFtZSI6InRhdWZpcV9pbXJvbiIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdLCJleHAiOjE3MjQ0NTgwODYsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiIsIlJPTEVfUkVBRCIsIlJPTEVfV1JJVEUiXSwianRpIjoiWl9kTXB5QmdLbEZZTGQzTnFRWDBUX1NxU21ZIiwiY2xpZW50X2lkIjoibXktY2xpZW50LXdlYiJ9.qvUklQg8msn-QOwagO_Z74Knna-2Aj4wYAbwQut_16c"
 
     // Function to provide the Bearer token
     fun getBearerToken(): String {
