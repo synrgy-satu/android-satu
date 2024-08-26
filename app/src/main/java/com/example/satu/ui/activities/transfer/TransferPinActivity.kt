90b393b85cef12eb9b09da958aef4333ebef26ac
pierreviences <imaniyazid@gmail.com>
2024-08-26 14:11:40 +0700

    fix: fixing bug pin transfer

--- app/src/main/java/com/example/satu/ui/activities/transfer/TransferPinActivity.kt
+++ app/src/main/java/com/example/satu/ui/activities/transfer/TransferPinActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -39,7 +40,7 @@ class TransferPinActivity : AppCompatActivity() {
 
     private val passwordList = mutableListOf<Int>() // To keep track of input
     private var isPasswordVisible = false // To track visibility state
-
+    private val correctPin = listOf(6, 6, 6, 6, 6, 6)
     private lateinit var binding : ActivityTransferPinBinding
     private val viewModel: TransferViewModel by viewModels {
         TransferViewModelfactory.getInstance(application)
@@ -151,51 +152,7 @@ class TransferPinActivity : AppCompatActivity() {
         tvShowPassword.setCompoundDrawablesWithIntrinsicBounds(drawableStart, null, null, null)
     }
 
-    private fun onLoginSuccess(data: DataTransfer) {
-        ProgressDialogUtils.hideProgressDialog()
 
-        val nominal = data.amount ?: 0
-        val referenceNumber = data.referenceNumber ?: 0
-        val waktuTransaksi = data.createdDate ?: ""
-        val note = data.note ?: ""
-        val namaPenerima = getSharedPreferences("UserTransferRekening", Context.MODE_PRIVATE)
-            .getString("namaRekeningTujuan", "") ?: ""
-        val rekeningTujuan = getSharedPreferences("UserTransferRekening", Context.MODE_PRIVATE)
-            .getString("targetRekeningNumber", "") ?: ""
-
-        val sharedPref = getSharedPreferences("UserProfile", Context.MODE_PRIVATE)
-        val fullName = sharedPref.getString("full_name", "")
-        val debitedRekeningNumber = sharedPref.getString("rekening_number", "")
-
-
-        val intent = Intent(this@TransferPinActivity, TransferReceiptActivity::class.java).apply {
-            putExtra("nominal", nominal.toString())
-            putExtra("waktu_transaksi", waktuTransaksi)
-            putExtra("nama_penerima", namaPenerima)
-            putExtra("rekening_tujuan", rekeningTujuan)
-            putExtra("rekening_pengirim", debitedRekeningNumber)
-            putExtra("nama_pengirim", fullName)
-            putExtra("note", note)
-            putExtra("referenceNumber", referenceNumber)
-        }
-
-        startActivity(intent)
-        clearSharedPreferences()
-        finish()
-    }
-    private fun onLoginError(errorMessage: String) {
-        ProgressDialogUtils.hideProgressDialog()
-        showSnackBar(errorMessage)
-    }
-    private fun showSnackBar(message: String) {
-        SnackbarUtils.showWithDismissAction(binding.root, message)
-    }
-
-    private fun clearSharedPreferences() {
-        getSharedPreferences("UserTransfer", Context.MODE_PRIVATE).edit().clear().apply()
-
-        getSharedPreferences("UserTransferRekening", Context.MODE_PRIVATE).edit().clear().apply()
-    }
     private fun validatePin(pin: String) {
         val sharedPrefToken = getSharedPreferences("UserToken", Context.MODE_PRIVATE)
         val tokens = sharedPrefToken.getString("token", "")
@@ -226,8 +183,6 @@ class TransferPinActivity : AppCompatActivity() {
 
                     val sharedPrefToken = getSharedPreferences("UserToken", Context.MODE_PRIVATE)
                     val token = sharedPrefToken .getString("token", "")
-
-
                         if (debitedRekeningNumber != null) {
                             if (targetRekeningNumber != null) {
                                 if (amount != null) {
@@ -252,20 +207,65 @@ class TransferPinActivity : AppCompatActivity() {
                             }
 //                    val intent = Intent(this@QrisVerificationActivity, QrisReceiptActivity::class.java)
 //                    startActivity(intent)
-                } else {
+                }
+                }else {
                     // Show an error message
                     Toast.makeText(this@TransferPinActivity, "PIN tidak valid", Toast.LENGTH_SHORT).show()
                     passwordList.clear()
                     updatePasswordViews()
                 }
             }
+
+
+            private fun onLoginSuccess(data: DataTransfer) {
+                ProgressDialogUtils.hideProgressDialog()
+
+                val nominal = data.amount ?: 0
+                val referenceNumber = data.referenceNumber ?: 0
+                val waktuTransaksi = data.createdDate ?: ""
+                val note = data.note ?: ""
+                val namaPenerima = getSharedPreferences("UserTransferRekening", Context.MODE_PRIVATE)
+                    .getString("namaRekeningTujuan", "") ?: ""
+                val rekeningTujuan = getSharedPreferences("UserTransferRekening", Context.MODE_PRIVATE)
+                    .getString("targetRekeningNumber", "") ?: ""
+
+                val sharedPref = getSharedPreferences("UserProfile", Context.MODE_PRIVATE)
+                val fullName = sharedPref.getString("full_name", "")
+                val debitedRekeningNumber = sharedPref.getString("rekening_number", "")
+
+
+                val intent = Intent(this@TransferPinActivity, TransferReceiptActivity::class.java).apply {
+                    putExtra("nominal", nominal.toString())
+                    putExtra("waktu_transaksi", waktuTransaksi)
+                    putExtra("nama_penerima", namaPenerima)
+                    putExtra("rekening_tujuan", rekeningTujuan)
+                    putExtra("rekening_pengirim", debitedRekeningNumber)
+                    putExtra("nama_pengirim", fullName)
+                    putExtra("note", note)
+                    putExtra("referenceNumber", referenceNumber)
+                }
+
+                startActivity(intent)
+                clearSharedPreferences()
+                finish()
+            }
+            private fun onLoginError(errorMessage: String) {
+                ProgressDialogUtils.hideProgressDialog()
+                showSnackBar(errorMessage)
+            }
+            private fun showSnackBar(message: String) {
+                SnackbarUtils.showWithDismissAction(binding.root, message)
             }
 
+            private fun clearSharedPreferences() {
+                getSharedPreferences("UserTransfer", Context.MODE_PRIVATE).edit().clear().apply()
 
-
+                getSharedPreferences("UserTransferRekening", Context.MODE_PRIVATE).edit().clear().apply()
+            }
 
             override fun onFailure(call: Call<PinValidationResponse>, t: Throwable) {
                 // Handle error
+                Log.d("error", t.message.toString())
                 Toast.makeText(this@TransferPinActivity, "Failed to validate PIN: ${t.message}", Toast.LENGTH_SHORT).show()
             }
         })
